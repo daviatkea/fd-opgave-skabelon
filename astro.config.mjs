e49f5878ce9a695c401cf75cea7259f5fcb3cc62
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-10-09 09:00:05 +0200

    feat(framework): add tailwind support to project
    See title.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "viewport",
   },
+
   image: {
     responsiveStyles: true,
     layout: "constrained",
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
